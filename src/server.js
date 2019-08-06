736716df5d307f261479e0f31d151fd4b78b4c7c
DiegoRamires <goncalves.ramires@gmail.com>
2019-08-06 18:00:52 -0300

    install and configure mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const server = express()
+
+mongoose.connect('mongodb+srv://nodeapi:rocketNodeDB!@cluster0-ahjnp.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+})
+
 server.use(express.json())
 server.use(routes)
 

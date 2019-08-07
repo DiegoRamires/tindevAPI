b5eff8e16401770338c6e70d104a3b30b88f4335
DiegoRamires <goncalves.ramires@gmail.com>
2019-08-06 22:09:20 -0300

    create action like

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,10 +1,24 @@
 const Dev = require('../models/Dev')
 
 module.exports = {
-  store(req, res) {
-    console.log(req.params.devId)
-    console.log(req.headers.user)
+  async store(req, res) {
+    // console.log(req.params.devId)
+    // console.log(req.headers.user)
 
-    return res.json({ ok: true })
+    const { user } = req.headers
+    const { devId } = req.params
+
+    const loggedDev = await Dev.findById(user)
+    const targetDev = await Dev.findById(devId)
+
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev not exists" })
+    }
+
+    loggedDev.likes.push(targetDev._id)
+
+    await loggedDev.save()
+
+    return res.json(loggedDev)
   }
 }
